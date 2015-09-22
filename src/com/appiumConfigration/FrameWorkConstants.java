a277a2b955bc805fa60b003fc4c6ef0b93434ddb
Sanoj <Sanoj@Sanoj-PC>
2015-09-22 17:50:56 +0530

    updated exit count

--- src/com/appiumConfigration/FrameWorkConstants.java
+++ src/com/appiumConfigration/FrameWorkConstants.java
@@ -100,7 +100,7 @@ public class FrameWorkConstants {
 			else
 			{
 				System.out.println("Enter "+errorLog.toString()+" in Configrtions!!");
-				System.exit(0);
+				System.exit(1);
 			}
 
 		}
