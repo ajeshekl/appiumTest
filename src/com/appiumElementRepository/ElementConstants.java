078ac185cee581e9c6dfb6dced358f6493111c45
sanoj <sanojs@rapidvaluesolutions.com>
2015-09-23 11:47:53 +0530

    Added Elements class

---
+++ src/com/appiumElementRepository/ElementConstants.java
@@ -0,0 +1,24 @@
+package com.appiumElementRepository;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ElementConstants {
+
+	public static Properties getElements()
+	{
+		Properties prop = new Properties();
+		InputStream input = null;
+		try
+		{
+			input = ElementConstants.class.getResourceAsStream("elements.properties");
+			prop.load(input);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return prop;
+	}
+	
+}
